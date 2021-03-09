43ea4c023529d4fabe688c9821e49bad94c2d438
Ajay-Git-Code <ajaysahani389852@gmail.com>
2021-03-09 17:25:45 +0530

    change and new function
    change main function and new function create

--- LearnGitHub/hello.cpp
+++ LearnGitHub/hello.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
+void fun();
 
 int main()
 {
-    std::cout<<"Hello World!";
+    std::cout<<"Hello World!"<<std::endl;
+    fun();
     return 0;
+}
+
+void fun()
+{
+    std::cout<<"new Function.";
 }
